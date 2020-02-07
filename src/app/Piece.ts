282ba257f0fa2c656327e84397840bf6fb01bbcc
ANDYDEARING <andy.dearing@gmail.com>
2020-02-07 13:36:56 -0500

    working pawn piece objects on init

--- src/app/Piece.ts
+++ src/app/Piece.ts
@@ -24,7 +24,7 @@ export class Piece{
             throw new TypeError("Invalid Piece Name: " + name);
         }
 
-        let regex = new RegExp('^[a-hA-H][1-8]$');
+        let regex = new RegExp('^[1-8][a-hA-H]$');
         if(regex.test(location)){
             this.location = location.toUpperCase();
         } else {
@@ -39,5 +39,8 @@ export class Piece{
         } else if (this.owner == "WHITE") {
             return this.whiteSymbols[this.pieceNames.indexOf(this.name)];
         }
+    }
+    public display(){
+        document.getElementById(this.location).innerText = this.symbol;
     }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PieceDefinition } from './PieceDefinition';
+import { Piece } from './Piece';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,14 @@ import { PieceDefinition } from './PieceDefinition';
 })
 export class AppComponent {
   title = 'ChessApp';
-  definer = new PieceDefinition();
+  // definer = new PieceDefinition();
+  whitePieces:Piece[] = new Array();
+  blackPieces:Piece[] = new Array();
+
+  ngOnInit(){
+    this.initializePieces();
+    this.displayPieces();
+  }
 
   onClick($event){
     //console.log($event);
@@ -17,4 +25,24 @@ export class AppComponent {
     //spaceElement.classList.add("border-red");
     let spaceElement: Element = $event.toElement;
   }
+
+  initializePieces(){
+    let currChar = 'A';
+    for(let i=0; i<8; i++){
+      this.whitePieces.push(new Piece("white","pawn","2" + currChar));
+      this.blackPieces.push(new Piece("black","pawn","7" + currChar));
+      currChar = nextChar(currChar);
+    }
+  }
+  displayPieces(){
+    for(let i=0;i<this.whitePieces.length;i++){
+      this.whitePieces[i].display();
+    }
+    for(let i=0;i<this.blackPieces.length;i++){
+      this.blackPieces[i].display();
+    }
+  }
+}
+function nextChar(c) {
+  return String.fromCharCode(c.charCodeAt(0) + 1);
 }
