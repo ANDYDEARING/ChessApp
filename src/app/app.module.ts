f92ff6a81cfdc5faaea823a6587384ae3b0a3925
Andy Dearing <andy.dearing@gmail.com>
2020-03-09 12:28:44 -0400

    added components for home and landing-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChessComponent } from './chess/chess.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChessComponent
+    ChessComponent,
+    LandingPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
