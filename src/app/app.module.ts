3173236599e4600ecd616d33d09fdd8e55f12daf
Andy Dearing <andy.dearing@gmail.com>
2020-03-31 13:45:24 -0400

    removed legacy chess compponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChessComponent } from './chess/chess.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -15,7 +14,6 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ChessComponent,
     LandingPageComponent,
     HomeComponent,
     GameComponent,
